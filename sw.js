7c6c75b4db2db7de39552b704021cfaf8d3aa57a
Moneythepro <107262811+Moneythepro@users.noreply.github.com>
2025-08-13 22:33:01 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,6 @@
-const CACHE_NAME = 'ludo-pwa-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './styles.css',
-  './app.js',
-  './manifest.webmanifest',
-  './assets/icons/icon-72.png',
-  './assets/icons/icon-96.png',
-  './assets/icons/icon-192.png',
-  './assets/icons/icon-512.png'
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
+const CACHE = 'ludo-lite-v1';
+const toCache = ['.','index.html','styles.css','app.js','manifest.webmanifest'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(toCache))); });
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
 });
